0f9c587fbe63a2251cf34e1d5297eeef242b85ca
ingeborgvpt <ingeborgvpt@gmail.com>
2026-09-24 12:44:49 +0200

    :)

--- js/script.js
+++ js/script.js
@@ -4,13 +4,10 @@
 
 // Eksempel: vi henter "on"-knappen ved hjælp af dens id-attribut (husk fra HTML/CSS: id identificerer ét bestemt element)
 const lightOn = document.getElementById("onBtn");
+
 const lightOff = document.getElementById("offBtn");
 
-// Skriv selv: hent "off"-knappen på samme måde, ved hjælp af dens id. Variablen skal hedde lightOff
-
-
-// Skriv selv: hent billedet på samme måde, ved hjælp af dets id. Variablen skal hedde bulb
-
+const bulb = document.getElementById("bulb");
 
 // Eksempel: denne funktion tænder lyset ved at ændre billedets kilde (src)
 function onBulb() {
@@ -19,8 +16,13 @@ function onBulb() {
 
 // Skriv selv en funktion, der hedder offBulb, og som slukker lyset ved at ændre billedets kilde (src) til "img/off.jpg"
 
+function offBulb() {
+  bulb.src = "img/off.jpg";
+}
+
 
 // Eksempel: vi lytter efter klik på "on"-knappen og kører onBulb, når der klikkes
 lightOn.addEventListener("click", onBulb);
+lightOff.addEventListener("click", offBulb);
 
 // Skriv selv koden, der lytter efter klik på "off"-knappen (lightOff) og kører din offBulb-funktion, når der klikkes
